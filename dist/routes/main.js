76b5cbc71de8498a72e3e81b0a232410b7ee8589
Marie Berezhna <marie.berezhna@gmail.com>
2024-08-17 16:11:39 +0200

    add public dir

--- dist/routes/main.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var mainRoute = (0, express_1.Router)();
-mainRoute.get('/', function (req, res) {
-    res.send('main api route works');
-});
-exports.default = mainRoute;
--- dist/routes/test.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var testRoute = (0, express_1.Router)();
-testRoute.get('/test', function (req, res) {
-    res.send('test api route works');
-});
-exports.default = testRoute;
